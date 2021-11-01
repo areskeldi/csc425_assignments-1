2d75aca2453573721fecb6089dba1f081719e0f4
emilbekuuluy <emilbekuuluy@csc4252021.ssdt.berea.edu>
2021-11-01 11:33:50 -0400

    add code for reserve_space and free_space

--- coding.freeList/freeList.cc
+++ coding.freeList/freeList.cc
@@ -1,21 +1,27 @@
 #include "freeList.h"
 
 freeList::freeList( long int*inRAM, int size ) {
+head=inRAM; //create a pointer to an array's element that is size
+head[0]=size-2; //create the first element of head that is size but have to substract 2 because that's the size of the header itself
+*next=NULL; //create the second element of head which is pointer to the next element in the free list
+head[1]=next;
 }
 
 long int*
 freeList::reserve_space( int size ) {
   long int* loc = NULL;
-
+  loc = head+4; //to return address of the allocation space
   return loc;
 }
 
 void
 freeList::free_space( long int* location ) {
+  head[1]=location-2; //setting next of the head to input location (2 before to start from metadata header)
 }
 
 void
 freeList::coalesce_forward() {
+if head[0]==0:
 }
 
 // print() is given to you. It traverses the free list and prints out what is there.
@@ -27,3 +33,4 @@ void freeList::print() {
     printHead = (long int *)printHead[1];
   }
 }
+
