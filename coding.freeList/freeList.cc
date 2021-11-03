f01c13fe32b2feb4fccb1c43b2a7572004ed4feb
emilbekuuluy <emilbekuuluy@csc4252021.ssdt.berea.edu>
2021-11-02 21:00:17 -0400

    add free space function

--- coding.freeList/freeList.cc
+++ coding.freeList/freeList.cc
@@ -3,27 +3,33 @@
 freeList::freeList( long int*inRAM, int size ) {
 head=inRAM; //create a pointer to an array's element that is size
 head[0]=size-2; //create the first element of head that is size but have to substract 2 because that's the size of the header itself
-*next=NULL; //create the second element of head which is pointer to the next element in the free list
-head[1]=next;
+head[1]=0; //create the second element of head which is pointer to the next element in the free list
 }
 
 long int*
 freeList::reserve_space( int size ) {
   long int* loc = NULL;
-  loc = head+4; //to return address of the allocation space
+  loc = head; //to return address of the allocation space
+  head = loc+size;
+  head[0]=loc[0]-size;
+  loc[0]=size;
   return loc;
 }
 
 void
 freeList::free_space( long int* location ) {
-  head[1]=location-2; //setting next of the head to input location (2 before to start from metadata header)
+  long int *next = head;
+  head = location;
+  head[1] = (long int)next;
 }
 
 void
 freeList::coalesce_forward() {
-if head[0]==0:
+  while(head[1] != NULL) {
+    long int *next = NULL;
+    next = (long int*)head[1];
+  }
 }
-
 // print() is given to you. It traverses the free list and prints out what is there.
 void freeList::print() {
   long int * printHead = head;
