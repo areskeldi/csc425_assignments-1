b0e94b843fe2e99d544f3e01ace6a89d7d671253
emilbekuuluy <emilbekuuluy@csc4252021.ssdt.berea.edu>
2021-11-05 12:19:52 -0400

    update coalesce

--- coding.freeList/freeList.cc
+++ coding.freeList/freeList.cc
@@ -9,25 +9,32 @@ head[1]=0; //create the second element of head which is pointer to the next elem
 long int*
 freeList::reserve_space( int size ) {
   long int* loc = NULL;
-  loc = head; //to return address of the allocation space
-  head = loc+size;
-  head[0]=loc[0]-size;
-  loc[0]=size;
+  loc = head + 2; //to return address of the allocation space
+  loc[0] = size; // saves the size of the space for future use based on input to the function, we need this when we free this space
+  head = loc+size; //updates the head pointer to now be where the reserved chunk ends!
+  head[0]=loc[0]-size; //makes the space of the updated free chunk to reflect that we just reserved space, so now the chunk has to be starting at the place where the inputted size ends
   return loc;
 }
 
 void
 freeList::free_space( long int* location ) {
-  long int *next = head;
-  head = location;
-  head[1] = (long int)next;
+  long int *next = head; //temporary variable to make sure we save the pointer of the current head for future use when declaring next for the head
+  head = location-2; //now we have a new head because we freed space
+  head[1] = (long int)next; // the header needs to point to the previous head
 }
 
 void
 freeList::coalesce_forward() {
-  while(head[1] != NULL) {
-    long int *next = NULL;
-    next = (long int*)head[1];
+  while(head[1] != 0) {
+    long int* ref2;
+    long int* ref2 = head + 2 + head[0];//temporary variable we need for future comparison to coalesce
+    long int* next;
+    next = (long int*)head[1];//temp variable for the chunk that will be joined to the chunk behind it
+
+   if(ref2 == next) {
+        head[0] = head[0] + 2 + next[0]; //updates the size of the newly coalesced chunk
+        head[1] = next[1]; //updates the next to be the next of the chunk ahead
+      }
   }
 }
 // print() is given to you. It traverses the free list and prints out what is there.
